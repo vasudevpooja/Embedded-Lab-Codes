24f6813d4741fcfec37b2aedd234a6d360fedbf2
Pooja Vasudev <poojavasudev.oct08@gmail.com>
2021-05-10 11:52:10 +0530

    Twister Counter

---
+++ Twisted Counter/twistcount.d
@@ -0,0 +1,7 @@
+.\twistcount.o: twistcount.c
+.\twistcount.o: C:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.H
+.\twistcount.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+.\twistcount.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\twistcount.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\twistcount.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\twistcount.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
